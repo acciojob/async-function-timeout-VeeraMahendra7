d7f42279e5a0eab53ada267d233296451ee7e257
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-24 11:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let input1 = document.getElementById('text');
+let input2 = document.getElementById('delay');
+let output = document.getElementById('output');
+
+function getData(text, delay) {
+	return new Promise((res,rej) => {
+		setTimeout(() => {
+			res(text);
+		}, delay);
+	})
+}
+
+async function display(){
+	let text = input1.value;
+	let delay = parseInt(input2.value);
+	let data = await getData(text,delay);
+	output.textContent = data;
+	text = "";
+	delay = "";
+}
+	
+
+document.getElementById('btn').addEventListener('click', display)
